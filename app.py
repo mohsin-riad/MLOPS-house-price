bc6e6300f80c881688e57aa9081f147efb61038c
mohsin-riad <morteen1997@gmail.com>
2023-02-03 01:53:12 +0600

    notebook and app updated

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import pickle
-import pandas
-import numpy 
+import json
+import pandas as pd
+import numpy as np
 from flask import Flask, request, app, jsonify, url_for, render_template
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
 model = pickle.load(open('./regression_model.pkl', 'rb'))
+sc = pickle.load(open('./scaler.pkl', 'rb'))
 
 @app.route('/')
 
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predict_api', methods=['POST'])
 
 def predict_api():
-    data = request.get_json(force=True)
-    print(data)
+    data = request.json['data']
+    mod_data = sc.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(mod_data)
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
