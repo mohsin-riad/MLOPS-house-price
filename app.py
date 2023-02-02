51db374bc6ca3c16214e4f77a840a9a7559220a1
mohsin-riad <morteen1997@gmail.com>
2023-02-03 04:02:39 +0600

    Backend Updated

--- app.py
+++ app.py
@@ -23,5 +23,12 @@ def predict_api():
     output = model.predict(mod_data)
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():  
+    data = [ float(i) for i in request.form.values()]
+    mod_data = sc.transform(np.array(data).reshape(1,-1))
+    y_pred = model.predict(mod_data)
+    return render_template('index.html', output = 'Prediction: {}'.format(round(y_pred[0],3)))
+
 if __name__=="__main__":
     app.run(debug=True)
